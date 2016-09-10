46f01e7042d53bc5efc81211ad7509e924374308
Jared Holzman <jared@holzman.io>
2016-09-10 15:58:27 -0400

    Fix so that html page is served

--- src/main/java/io/holzman/Application.java
+++ src/main/java/io/holzman/Application.java
@@ -2,15 +2,17 @@ package io.holzman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by jared on 9/10/16.
  */
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/io/holzman/controllers/WebViewController.java
+++ src/main/java/io/holzman/controllers/WebViewController.java
@@ -1,14 +1,19 @@
 package io.holzman.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by jared on 9/10/16.
  */
+
+@Controller
 public class WebViewController {
-    @RequestMapping(method = RequestMethod.GET, value = "/")
+    @RequestMapping(method = RequestMethod.GET, value = "/web")
     public String index(){
+        ModelAndView modelAndView = new ModelAndView("index");
         return "index";
     }
 }
