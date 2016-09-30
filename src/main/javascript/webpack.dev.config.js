955564a98b45a704cc187619d0aaf60bbdf5a48a
Jared Holzman <jared@holzman.io>
2016-09-29 22:26:48 -0400

    Improved project config

--- src/main/javascript/webpack.dev.config.js
+++ src/main/javascript/webpack.dev.config.js
@@ -1,6 +1,5 @@
 var path = require('path');
 var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var SRC = path.join(__dirname, 'index.js');
 var DEST = path.join(__dirname, '../resources/static/dist/');
@@ -9,32 +8,36 @@ const PUBLPATH = 'http://localhost:3000/bundle/';
 module.exports = {
     devtool: 'eval',
     entry: [
-        'react-hot-loader/patch',
         'webpack-dev-server/client?http://localhost:3000',
         'webpack/hot/only-dev-server',
-        './index.js'
+        './src/index'
     ],
-    resolve: {
-        extensions: ['', '.js']
-    },
     output: {
-        path: path.join(__dirname, '../resources/static/dist'),
+        path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/static/'
     },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
-        loader: [{
+        loaders: [{
             test: /\.js$/,
-            loaders: ['react-hot', 'jsx?harmony'],
-            include: SRC
+            loaders: ['babel'],
+            include: path.join(__dirname, 'src')
         }]
     },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify('development')
+    devServer: {
+        inline: true,
+        hot: true,
+        port: 3000,
+        proxy: {
+            '/**': {
+                target: 'http://localhost:8080',
+                secure: false,
+                // node-http-proxy option - don't add /localhost:8080/ to proxied request paths
+                prependPath: false
             }
-        })
-    ]
+        }
+    }
 };
