d21ed21b5bf6d56a5eb2f412335872cffa1e9927
Jared Holzman <jared@holzman.io>
2016-10-07 17:38:16 -0400

    Remove some uneccessary stuff form index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import initStore from 'config/store';
-import { setupAxiosInterceptors } from 'rest/axios';
 import DevTools from 'config/devtools';
-import { redirectToLoginWithMessage, logout } from 'reducers/authentication';
 import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
@@ -17,14 +14,11 @@ const devTools = process.env.NODE_ENV === 'development' ? <DevTools /> : null;
 const store = initStore();
 const history = syncHistoryWithStore(browserHistory, store);
 
-const actions = bindActionCreators({redirectToLoginWithMessage, logout}, store.dispatch);
-setupAxiosInterceptors(() => actions.redirectToLoginWithMessage('login.error.unauthorized'));
-
 ReactDOM.render(
     <Provider store={store}>
         <div>
             {devTools}
-            <Router history={history} routes={getRoutes(actions.logout)}/>
+            <Router history={history} routes={getRoutes()}/>
         </div>
     </Provider>,
     document.getElementById('root')
