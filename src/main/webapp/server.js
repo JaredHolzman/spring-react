8f7cf8784d863fb1470523afeded6a8dd0bfd06a
Jared Holzman <jared@holzman.io>
2016-10-08 01:48:45 -0400

    Updated babel mod resolver, remove history connect

--- src/main/webapp/server.js
+++ src/main/webapp/server.js
@@ -2,13 +2,11 @@ var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
 var config = require('./webpack.dev.config');
-var history = require('connect-history-api-fallback');
 var proxy = require('http-proxy-middleware');
 
 var app = express();
 var compiler = webpack(config);
 
-app.use(history());
 app.use(proxy('/api', {target: 'http://localhost:8080', changeOrigin: true}));
 
 app.use(require('webpack-dev-middleware')(compiler, {
