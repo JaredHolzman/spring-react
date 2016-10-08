ab7b5eee36a7835d08e4bcaa80640b58f976e52c
Jared Holzman <jared@holzman.io>
2016-10-08 00:55:54 -0400

    Removed uneeded import from App

--- src/main/webapp/src/ui/container/App.js
+++ src/main/webapp/src/ui/container/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
-import counterpart from 'counterpart';
 
 import 'stylus/main.styl';
 
