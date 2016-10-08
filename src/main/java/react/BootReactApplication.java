0cdf47cde9f47374035be0519f8ac24f2d7acbdb
Jared Holzman <jared@holzman.io>
2016-10-08 02:43:49 -0400

    Remove class exclusion from Application

--- src/main/java/react/BootReactApplication.java
+++ src/main/java/react/BootReactApplication.java
@@ -2,9 +2,9 @@ package react;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 
-@SpringBootApplication(exclude = SessionAutoConfiguration.class)
+
+@SpringBootApplication
 public class BootReactApplication {
 
   public static void main(String[] args) {
