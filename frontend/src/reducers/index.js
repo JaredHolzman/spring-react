e6648121cf930aa86c68dddd700c434a428dee87
Jared Holzman <jared@holzman.io>
2016-10-07 22:32:03 -0400

    Fixed complex url hot-loading bug

---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import simple from './simple';
+import locale from './locale';
+import { routerReducer as routing } from 'react-router-redux';
+
+export default combineReducers({
+  simple,
+  locale,
+  routing
+});
---
+++ frontend/src/ui/container/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import counterpart from 'counterpart';
+
+import 'stylus/main.styl';
+
+const TopMenu = (props) => {
+  const items = props.items.map((item, key) => (
+    <li key={key} className="pure-menu-item">
+      <Link to={item.link} className="pure-menu-link">{item.label}</Link>
+    </li>
+  ));
+  return (
+    <div className="pure-menu pure-menu-horizontal">
+      <ul className="pure-menu-list">
+        {items}
+      </ul>
+    </div>
+  );
+};
+
+export class App extends Component {
+
+  componentDidMount() {
+    this.props.getSession();
+  }
+
+  render() {
+    const menuItems = [
+      {label: 'Home', link: '/'},
+      {label: 'Hello', link: '/hello'},
+      {label: 'Goodbye', link: '/goodbye'}
+    ];
+
+    return (
+      <div id="application">
+        <TopMenu items={menuItems} />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({}),
+  {}
+)(App);
