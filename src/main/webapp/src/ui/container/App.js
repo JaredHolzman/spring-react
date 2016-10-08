62de6e3e8becc2c704d55fc551b625278c1e4aae
Jared Holzman <jared@holzman.io>
2016-10-08 04:09:40 -0400

    Add react-bootstrap

--- src/main/webapp/src/ui/container/App.js
+++ src/main/webapp/src/ui/container/App.js
@@ -1,48 +1,30 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router';
 import {connect} from 'react-redux';
 import {fetchStub} from 'actions/stub';
-
-import 'stylus/main.styl';
-
-const TopMenu = (props) => {
-    const items = props.items.map((item, key) => (
-        <li key={key} className="pure-menu-item">
-            <Link to={item.link} className="pure-menu-link">{item.label}</Link>
-        </li>
-    ));
-    return (
-        <div className="pure-menu pure-menu-horizontal">
-            <ul className="pure-menu-list">
-                {items}
-            </ul>
-        </div>
-    );
-};
+import FetchDataComponent from 'component/FetchDataComponent';
+import TopNav from 'component/TopNav';
 
 export class App extends Component {
 
-    componentWillMount(){
-        this.props.fetchStub();
-    }
-
     render() {
-        const menuItems = [
-            {label: 'Home', link: '/'},
-            {label: 'Hello', link: '/hello'},
-            {label: 'Goodbye', link: '/goodbye'}
-        ];
 
         return (
             <div id="application">
-                <TopMenu items={menuItems}/>
+                <TopNav/>
                 {this.props.children}
+                <FetchDataComponent
+                    fetchData={() => this.props.fetchStub()}
+                    data={this.props.data}/>
             </div>
         );
     }
 }
 
-const maStateToProps = (state) => {};
+const maStateToProps = (state) => {
+    return {
+        data: state.stub
+    };
+};
 const mapDispatchToProps = {fetchStub};
 
-export default connect(state => ({}), {fetchStub})(App);
+export default connect((state) => maStateToProps(state), mapDispatchToProps)(App);
